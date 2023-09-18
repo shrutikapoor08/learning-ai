43de8cff3baa5cd7e38d49c55f190ef1c373e605
shrutikapoor08 <shruti.kapoor08@gmail.com>
2023-09-17 17:24:51 -0700

    WIP. Adding backend, frontend and llm

--- llm.js
+++ llm.js
@@ -1,6 +1,17 @@
 import { PromptTemplate } from "langchain/prompts";
 import { OpenAI } from "langchain/llms/openai";
 import { StructuredOutputParser } from "langchain/output_parsers";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { LLMChain } from "langchain/chains";
+import {
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} from "langchain/prompts";
+import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 
 import "dotenv/config";
 
@@ -9,10 +20,14 @@ const llm = new OpenAI({
 });
 
 const parser = StructuredOutputParser.fromNamesAndDescriptions({
-  price_ending: "Ending range of price of properties",
-  price_starting: "Starting range of price of properties",
-  bedrooms: "Number of bedrooms",
-  cities: "comma separated list of cities or neighborhoods to view",
+  price_ending: "Ending range of price of properties. Parse as a number",
+  price_starting:
+    "Starting range of price of properties. Parse as a number. If not present, add 500,000",
+  bedrooms: "Number of bedrooms as an integer",
+  wants:
+    "any should haves in the property. add none if none mentioned. if mentioned, parse them as a comma separated list",
+  // neighborhoods:
+  //   "comma separated list of neighborhoods in the text such as seattle, botthel",
   // requirements:
   //   "Figure out the must haves of the property. Add as a comma separated list",
 });
@@ -29,7 +44,7 @@ const prompt = new PromptTemplate({
 const model = new OpenAI({ temperature: 0 });
 
 const description =
-  "I want to buy a single family home in Seattle, Ballard, Shoreline, area starting from 500000 to 1 million. It needs to have 3 bedrooms, a frontyard, a backyard, and be close to shopping and restaurants. It would be nice to have is a fireplace and have a nice view";
+  "I need a single family home in the city of Seattle, Ballard, Shoreline, Bothell neighborhoods starting from 900000 to 1.2 million. also need 3 bedrooms. I want it to have a fireplace, garage, backyard";
 
 // https://js.langchain.com/docs/modules/model_io/output_parsers/
 const input = await prompt.format({ description: description });
@@ -37,3 +52,63 @@ const input = await prompt.format({ description: description });
 const response = await model.call(input);
 
 console.log(response);
+
+// const zodSchema = z.object({
+//   properties: z
+//     .array(
+//       z.object({
+//         price_ending: z
+//           .string()
+//           .describe("Ending range of price of properties"),
+//         price_starting: z
+//           .boolean()
+//           .describe("Starting range of price of properties"),
+//         bedrooms: z.string().optional().describe("Number of bedrooms wanted"),
+//       })
+//     )
+//     .describe("An array of requirements mentioned in the text"),
+// });
+
+// // const outputFixingParser = OutputFixingParser.fromLLM(chatModel, outputParser);
+
+// const prompt = new ChatPromptTemplate({
+//   promptMessages: [
+//     SystemMessagePromptTemplate.fromTemplate(
+//       "List all requirements mentioned in the following text."
+//     ),
+//     HumanMessagePromptTemplate.fromTemplate("{propertyDescription}"),
+//   ],
+//   inputVariables: ["propertyDescription"],
+// });
+
+// const llm = new ChatOpenAI({
+//   key: process.env.OPENAI_API_KEY,
+//   modelName: "gpt-3.5-turbo-0613",
+//   temperature: 0,
+// });
+
+// // Binding "function_call" below makes the model always call the specified function.
+// // If you want to allow the model to call functions selectively, omit it.
+// // const functionCallingModel = llm.bind({
+// //   functions: [
+// //     {
+// //       name: "output_formatter",
+// //       description: "Should always be used to properly format output",
+// //       parameters: zodToJsonSchema(zodSchema),
+// //     },
+// //   ],
+// //   function_call: { name: "output_formatter" },
+// // });
+
+// const outputParser = new JsonOutputFunctionsParser();
+
+// // const chain = prompt.pipe(functionCallingModel).pipe(outputParser);
+
+// const response = await await llm.call();
+
+// // prompt.format({
+// //   propertyDescription:
+// //     "I want to buy a single family home in Seattle, Ballard, Shoreline, Bothell starting from 500000 to 1 million. 3 bedrooms needed",
+// // });
+
+// console.log(JSON.stringify(response, null, 2));
--- server.js
+++ server.js
@@ -1,80 +1,33 @@
-import { PromptTemplate } from "langchain/prompts";
-import { LLMChain } from "langchain/chains";
-import { OpenAI } from "langchain/llms/openai";
-import { initializeAgentExecutorWithOptions } from "langchain/agents";
-import { SerpAPI } from "langchain/tools";
-import { Calculator } from "langchain/tools/calculator";
+import express from "express";
+// import testAPI from "./client.js";
+import fs from "fs";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import path from "path";
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
 
-import "dotenv/config";
+const __dirname = path.resolve(path.dirname(""));
 
-const model = new OpenAI({ temperature: 0 });
-const tools = [
-  new SerpAPI(process.env.SERPAPI_API_KEY, {
-    location: "Austin,Texas,United States",
-    hl: "en",
-    gl: "us",
-  }),
-  new Calculator(),
-];
-const executor = await initializeAgentExecutorWithOptions(tools, model, {
-  agentType: "zero-shot-react-description",
+app.use(bodyParser.json());
+app.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-const input =
-  "Who is Olivia Wilde's boyfriend?" +
-  " What is his current age raised to the 0.23 power?";
-console.log(`Executing with input "${input}"...`);
+app.post("/parse-properties", function (req, res) {
+  console.log("in parse properties post");
+  const newUser = {
+    first_name: "shruti",
+    last_name: "kapoor",
+  };
+  response.status(201).json(newUser);
+});
 
-const result = await executor.call({ input });
-console.log({ result });
-
-// import axios from "axios";
-// import express from "express";
-// let houses = {};
-// const options = {
-//   method: "GET",
-//   url: "https://zillow56.p.rapidapi.com/search",
-//   params: {
-//     location: "seattle, wa",
-//     status: "forSale",
-//     sortSelection: "priced",
-//     isSingleFamily: "true",
-//     price_max: "1100000",
-//     beds_min: "3",
-//     isApartment: "false",
-//     isCondo: "false",
-//     isTownhouse: "false",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": "1082b29331mshf1c50763d566794p1aa9ffjsne135989b6a1b",
-//     "X-RapidAPI-Host": "zillow56.p.rapidapi.com",
-//   },
-// };
-// // const express = require("express");
-// const app = express();
-// const port = 3000;
-
-// app.get("/", function (req, res) {
-//   let response;
-//   let houses;
-//   const fetchData = async () => {
-//     response = await axios.request(options);
-//     houses = response.data;
-
-//     // const em = document.createElement("em");
-//     // const para = document.querySelector("p");
-
-//     // em.textContent = houses[0].streetAddress;
-//     // para.appendChild(em);
-//   };
-
-//   fetchData();
-//   res.json(response);
+// app.get("/parse-properties", function (req, res) {
+//   console.log("in parse properties");
 // });
 
-// app.listen(port, function () {
-//   console.log(`Example app listening on port ${port}!`);
-// });
-
-// export default app;
-// // module.exports = app;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
