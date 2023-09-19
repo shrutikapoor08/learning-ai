e522f38c8794a87ce8207539d4657b0a21586215
shrutikapoor08 <shruti.kapoor08@gmail.com>
2023-09-19 00:03:13 -0700

    adding backend and frontend

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 // import testAPI from "./client.js";
 import fs from "fs";
 import { dirname } from "path";
-import bodyParser from "body-parser";
 import path from "path";
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,23 +9,20 @@ const router = express.Router();
 
 const __dirname = path.resolve(path.dirname(""));
 
-app.use(bodyParser.json());
-app.use("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
-});
+app.use(express.json({ strict: false }));
 
 app.post("/parse-properties", function (req, res) {
-  console.log("in parse properties post");
-  const newUser = {
-    first_name: "shruti",
-    last_name: "kapoor",
-  };
-  response.status(201).json(newUser);
+  console.log("in parse properties post", req.body);
+  res.json(`Here is what you sent me: ${req.body.post}`);
 });
 
 // app.get("/parse-properties", function (req, res) {
 //   console.log("in parse properties");
 // });
+
+app.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
