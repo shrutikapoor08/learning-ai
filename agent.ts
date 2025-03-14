265f12d4ab84621efdba05eb212079eb565007ed
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-03-13 22:53:25 -0700

    Adding agent

---
+++ agent.ts
@@ -0,0 +1,41 @@
+// agent.ts
+
+// IMPORTANT - Add your API keys here. Be careful not to publish them.
+import "dotenv/config";
+
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { ChatOpenAI } from "@langchain/openai";
+import { MemorySaver } from "@langchain/langgraph";
+import { HumanMessage } from "@langchain/core/messages";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+
+// Define the tools for the agent to use
+const agentTools = [new TavilySearchResults({ maxResults: 3 })];
+const agentModel = new ChatOpenAI({ temperature: 0, apiKey: process.env.OPENAI_API_KEY });
+
+// Initialize memory to persist state between graph runs
+const agentCheckpointer = new MemorySaver();
+const agent = createReactAgent({
+  llm: agentModel,
+  tools: agentTools,
+  checkpointSaver: agentCheckpointer,
+});
+
+// Now it's time to use!
+const agentFinalState = await agent.invoke(
+  { messages: [new HumanMessage("what is the walk score and weather of salesforce park in ny?")] },
+  { configurable: { thread_id: "42" } },
+);
+
+console.log(
+  agentFinalState.messages[agentFinalState.messages.length - 1].content,
+);
+
+const agentNextState = await agent.invoke(
+  { messages: [new HumanMessage("what about salesforce park in ny")] },
+  { configurable: { thread_id: "42" } },
+);
+
+console.log(
+  agentNextState.messages[agentNextState.messages.length - 1].content,
+);
